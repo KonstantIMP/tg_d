c51136dbd68931c7657a5d3df428e0725820b9af
KonstantIMP <mihedovkos@gmail.com>
2021-08-01 23:43:52 +0300

    Add text formats

---
+++ source/tg/core/format.d
@@ -0,0 +1,15 @@
+/**
+ * Supported text format style
+ * Author: KonstantIMP <mihedovkos@gmail.com>
+ * Date: 1 Aug 2021
+ */
+module tg.core.format;
+
+/** 
+ * Supported text format styles
+ */
+enum TextFormat : string {
+    MarkdownV2 = "MarkdownV2",
+    HTML       = "HTML",
+    Markdown   = "Markdown"
+}
